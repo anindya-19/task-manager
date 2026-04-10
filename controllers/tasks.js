3d4706090b335ca7ba7f8e02458dff8bd98eaf06
Anindya Ganguly <anindyaganguly19@gmail.com>
2026-04-10 13:01:13 +0530

    made deleteTask controller function functional

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -3,7 +3,7 @@ const Task = require("../models/Task");
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({});
-    res.status(201).json({ tasks });
+    res.status(200).json({ tasks });
   } catch (error) {
     res.status(500).json({ msg: error });
   }
@@ -19,8 +19,13 @@ const createTasks = async (req, res) => {
 const getTask = async (req, res) => {
   try {
     const { id: taskID } = req.params;
+    //This extracts the id property from req.params but immediately saves it into a brand new variable named taskID. It just makes the rest of the code slightly easier to read.
+
     const task = await Task.findOne({ _id: taskID });
+    //MongoDB automatically names its ID fields _id. You are telling it to find the document where the _id matches the taskID you pulled from the URL.
+
     if (!task) {
+      //findOne() returns null if the given id is not found
       return res.status(404).json({ msg: `To task with id: ${taskID}` });
     }
     res.status(200).json({ task });
@@ -28,11 +33,25 @@ const getTask = async (req, res) => {
     res.status(500).json({ error });
   }
 };
+
 const updateTask = (req, res) => {
   res.send("update a task");
 };
-const deleteTask = (req, res) => {
-  res.send("Delete task");
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+
+    const task = await Task.findOneAndDelete({ _id: taskID });
+
+    if (!task) {
+      return res.status(404).json({ msg: `Not task with id ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
